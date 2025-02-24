2ce5d6f2e24f019dfcd3123dc8fd5b07f2c1133b
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-02-24 05:44:22 +0000

    Revert "chore: 라우터 기반 코드 스플리팅 제거하여 테스트"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { Suspense } from "react";
+import { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -6,32 +6,30 @@ import { GlobalStyle } from "styles";
 import { useForegroundNotification } from "hooks";
 import { Modal } from "components/organisms";
 import { useModalStore } from "stores";
-// import { Loading } from "components/molecules/Loading";
+import { Loading } from "components/molecules/Loading";
 import { router } from "router";
 const queryClient: QueryClient = new QueryClient();
 
 const App = () => {
   useForegroundNotification();
-  const isOpen = useModalStore(store => store.isOpen);
-  const content = useModalStore(store => store.content);
-  const { closeModal } = useModalStore(store => store.actions);
+  const isOpen = useModalStore((store) => store.isOpen);
+  const content = useModalStore((store) => store.content);
+  const { closeModal } = useModalStore((store) => store.actions);
 
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyle />
-      {/* <Suspense fallback={<Loading />}> */}
-      <RouterProvider
-        router={router}
-        future={{
-          v7_startTransition: true,
-        }}
-      />
-      {/* </Suspense> */}
+      <Suspense fallback={<Loading />}>
+        <RouterProvider
+          router={router}
+          future={{
+            v7_startTransition: true,
+          }}
+        />
+      </Suspense>
       <Modal open={isOpen} onClose={closeModal}>
         <Modal.Background hasClickEvent />
-        <Modal.Container>
-          {content}
-        </Modal.Container>
+        <Modal.Container>{content}</Modal.Container>
       </Modal>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
