fb79f6d85994bc1beefc9b72d1f76c681dd29b7e
JW-Ahn0 <jun.woo.an0@gmail.com>
2025-02-27 04:44:29 +0000

    chore: requestPermission 없이 토큰 발급

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -5,20 +5,24 @@ export const useNotification = () => {
   /**
    * 알림 권한을 확인하여 토큰을 발급하는 함수
    */
-  const getFcmToken = async (): Promise<string> => {
+  const getFcmToken = async () => {
     try {
-      const permission = await Notification.requestPermission();
-      if (permission === "granted") {
-        const token = await getToken(messaging, {
-          //   vapidKey: "BLS2Yc1y5SwiFSU00YiHwhBLMDcwQen-G-z9R7Q-79CpsJ7tuac_y9R3zlRz1x6u81qWrGlk9n2qyitw7ZrbrKQ",
-          vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
-        });
-        console.log("FCM 토큰:", token);
+      const token = await getToken(messaging, {
+        //   vapidKey: "BLS2Yc1y5SwiFSU00YiHwhBLMDcwQen-G-z9R7Q-79CpsJ7tuac_y9R3zlRz1x6u81qWrGlk9n2qyitw7ZrbrKQ",
+        vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+      });
+      if (token) {
         return token;
-      } else {
-        console.warn("알림 권한이 거부되었습니다.");
-        return "";
       }
+      // const permission = await Notification.requestPermission();
+      // if (permission === "granted") {
+
+      //   console.log("FCM 토큰:", token);
+      //   return token;
+      // } else {
+      //   console.warn("알림 권한이 거부되었습니다.");
+      //   return "";
+      // }
     } catch (error) {
       console.error("토큰 발급 실패:", error);
       return "";
